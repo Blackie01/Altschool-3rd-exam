573aa46a223778e86ecb18e6b7144482f74ba2d6
Blackie01 <damola24795@gmail.com>
2023-03-20 08:23:48 +0100

    more styling

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
-  
   <!-- <router-link to="/counter"> <h1>sjhvskjf</h1> </router-link> -->
   <router-view />
   <CounterPage />
@@ -18,34 +17,61 @@ components: {
 </script>
 
 <style>
+
 html {
+/* flex-direction: column; */
 height: 100vh;
   display: grid;
   align-content: center;
   /* justify-content: center; */
 }
 body {
-background-color: #013d3bff;
+
+/* background-color: #013d3bff; */
+background: linear-gradient(120deg, #272626 37%, #673736, #db5a56);
+
 /* background-color: #012928ff; */
 }
+/* #3f87a6 
+#673736
+*/
+
+.jumboName {
+  font-size: 100px;
+  margin-left: 90px;
+  color: #db5a56;
+}
+
+.dot {
+  color: #774140;
+}
+
+#tagline {
+  margin-top: -80px;
+}
+
 #app {
 font-family: Avenir, Helvetica, Arial, sans-serif;
 -webkit-font-smoothing: antialiased;
 -moz-osx-font-smoothing: grayscale;
 text-align: center;
 color: #2c3e50;
-background-color: #012928ff;
+/* background-color: #012928ff; */
+/* background-color: blue; */
 /* margin-top: 60px; */
   width: 53%;
   height: 18em;
-  padding: 22px 0 5px 0;
+  /* padding: 2px 0 5px 0; */
   position: relative;
-  left: 23%;
+  left: 40%;
+  bottom: 25%;
   /* width: 125%; */
 }
+
 h1, h3 {
-color: white;
+color: #eedada;
 }
+
 @media screen and (max-width: 768px) {
 #app {
   width: 100%;
--- src/components/CounterPage.vue
+++ src/components/CounterPage.vue
@@ -1,72 +1,92 @@
 <template>
-    <div class="container">
-      <h1> My Counter App</h1>
-      <h3> Count: </h3>
-      <span> {{ count }} </span> 
-      <div class="buttons">
-        <button @click="increment">+</button>
+  <div class="container">
+    <h3> how many claps for 'Her'?</h3>
+    <span class="defaultCount"> {{ count }} </span>
+    <div class="buttons">
+      <button @click="increment">+</button>
       <button @click="decrement">-</button>
       <button @click="setValue(25)">Set to 25</button>
       <button @click="setValue(50)">Set to 50</button>
-      </div>
-      <button class="reset" @click="reset">Reset</button>
     </div>
-  </template>
+    <button class="reset" @click="reset">Reset</button>
+  </div>
+</template>
   
-  <script>
-  import useCounter from '@/composables/useCounter'
-  
-  export default {
-    setup() {
-      const { count, increment, decrement, reset, setValue } = useCounter()
-  
-      return {
-        count,
-        increment,
-        decrement,
-        reset,
-        setValue
-      }
+<script>
+import useCounter from '@/composables/useCounter'
+
+export default {
+  setup() {
+    const { count, increment, decrement, reset, setValue } = useCounter()
+
+    return {
+      count,
+      increment,
+      decrement,
+      reset,
+      setValue
     }
   }
-  </script>
+}
+</script>
 
-  <style>
-  @import url('https://fonts.googleapis.com/css2?family=Mulish:wght@200&family=Stardos+Stencil:wght@700&display=swap');
-    .container {
-      background-color: #012928ff;
-    }
-    .buttons {
-      display: grid;
-    grid-template-columns: auto auto;
-    justify-content: center;
-    grid-gap: 10px;
-    margin-bottom: 15px;
-    grid-gap: 13px;
-    margin-bottom: 15px;
-    margin-top: 20px;
-    }
-    button {
-      width: 8rem;
-      background-color: #013d3bff;
-      height: 27px;
-      border-color: #2c3e50;
-      cursor: pointer;
-      color: white;
-    }
-    button:hover {
-      color: black;
-    }
-    .reset {
-      width: 20em;
-    } 
-    span {
-      font-family: 'Mulish', sans-serif;
-      font-family: 'Stardos Stencil', cursive;
-      color: white;
-      font-size: 20px;
-    }
-    h3, h5 {
-      margin: 5px 0 15px 0;
-    }
-  </style>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Mulish:wght@200&family=Stardos+Stencil:wght@700&display=swap');
+
+.defaultCount {
+  font-size: 50px;
+  color: #eedada;
+}
+
+.container {
+  background: linear-gradient(120deg, #272626 37%, #673736, #db5a56);
+  /* background-color: #d9df7e;  */
+  padding-top: 40px;
+  padding-bottom: 60px;
+  border-radius: 20px;
+  z-index: 1;
+}
+
+.buttons {
+  display: grid;
+  grid-template-columns: auto auto;
+  justify-content: center;
+  grid-gap: 10px;
+  margin-bottom: 15px;
+  grid-gap: 13px;
+  margin-bottom: 15px;
+  margin-top: 20px;
+}
+
+button {
+  width: 8rem;
+  background-color: #774140;
+  /* background-color: #dfb27e; */
+  height: 27px;
+  /* border-color: #2c3e50; */
+  cursor: pointer;
+  color: #eedada;
+  border-radius: 5px;
+}
+
+button:hover {
+  color: #d81616;
+}
+
+.reset {
+  width: 20em;
+}
+
+span {
+  font-family: 'Mulish', sans-serif;
+  font-family: 'Stardos Stencil', cursive;
+  color: white;
+  font-size: 20px;
+}
+
+h3,
+h5 {
+  margin: 5px 0 15px 0;
+  font-family: 'Righteous', cursive;
+}
+</style>
